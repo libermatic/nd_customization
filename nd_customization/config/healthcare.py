ca2b3e35565f175af0d2dabc1ac925415c69e3ef
Sun Howwrongbum <sun@libermatic.com>
2018-12-04 13:12:20 +0530

    Lab Test Center: create this

---
+++ nd_customization/config/healthcare.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from frappe import _
+
+
+def get_data():
+    return [
+        {
+            'label': _('Masters'),
+            'items': [
+                {
+                    'type': 'doctype',
+                    'name': 'Lab Test Center',
+                    'label': 'Test Center',
+                },
+            ]
+        },
+    ]
