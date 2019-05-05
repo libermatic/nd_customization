cfffcb505ad2c53b622197f80278951ec79b4c52
Sun Howwrongbum <sun@libermatic.com>
2019-05-05 22:48:16 +0530

    Item-wise Daily Sales and Commissions: create this

---
+++ nd_customization/nd_customization/report/item_wise_daily_sales_and_commissions/item_wise_daily_sales_and_commissions.js
@@ -0,0 +1,38 @@
+// Copyright (c) 2016, Libermatic and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports['Item-wise Daily Sales and Commissions'] = {
+  filters: [
+    {
+      fieldname: 'from_date',
+      label: 'From Date',
+      fieldtype: 'Date',
+      default: frappe.datetime.get_today(),
+    },
+    {
+      fieldname: 'to_date',
+      label: 'To Date',
+      fieldtype: 'Date',
+      default: frappe.datetime.get_today(),
+    },
+    {
+      fieldname: 'patient',
+      label: 'Patient ID',
+      fieldtype: 'Link',
+      options: 'Patient',
+    },
+    {
+      fieldname: 'physician',
+      label: 'Ref Physician',
+      fieldtype: 'Link',
+      options: 'Physician',
+    },
+    {
+      fieldname: 'sales_partner',
+      label: 'Sales Partner',
+      fieldtype: 'Link',
+      options: 'Sales Partner',
+    },
+  ],
+};
---
+++ nd_customization/nd_customization/report/item_wise_daily_sales_and_commissions/item_wise_daily_sales_and_commissions.py
@@ -0,0 +1,131 @@
+# Copyright (c) 2013, Libermatic and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe import _
+from datetime import datetime
+from functools import partial
+from toolz import compose, pluck, keyfilter, merge
+
+
+def execute(filters=None):
+    columns, data = _get_columns(), _get_data(filters)
+    return columns, map(_pick_fields(columns), data)
+
+
+def _pick_fields(columns):
+    keys = compose(list, partial(pluck, "fieldname"))(columns)
+    return partial(keyfilter, lambda k: k in keys)
+
+
+def _get_columns():
+    return [
+        {
+            "label": _("Sales Invoice"),
+            "fieldname": "sales_invoice",
+            "fieldtype": "Link",
+            "options": "Sales Invoice",
+            "width": 120,
+        },
+        {
+            "label": _("Patient ID"),
+            "fieldname": "patient",
+            "fieldtype": "Link",
+            "options": "Patient",
+            "width": 90,
+        },
+        {
+            "label": _("Patient Name"),
+            "fieldname": "patient_name",
+            "fieldtype": "Data",
+            "width": 120,
+        },
+        {
+            "label": _("Time"),
+            "fieldname": "posting_datetime",
+            "fieldtype": "Datetime",
+            "width": 150,
+        },
+        {
+            "label": _("Amount"),
+            "fieldname": "amount",
+            "fieldtype": "Currency",
+            "width": 90,
+        },
+        {
+            "label": _("Net Amount"),
+            "fieldname": "net_amount",
+            "fieldtype": "Currency",
+            "width": 90,
+        },
+        {
+            "label": _("Referring Physician"),
+            "fieldname": "physician",
+            "fieldtype": "Link",
+            "options": "Physician",
+            "width": 120,
+        },
+        {
+            "label": _("Sales Partner"),
+            "fieldname": "sales_partner",
+            "fieldtype": "Link",
+            "options": "Sales Partner",
+            "width": 120,
+        },
+    ]
+
+
+def _get_data(filters):
+    data = frappe.db.sql(
+        """
+            SELECT
+                sii.item_code AS test_code,
+                sii.item_name AS test_name,
+                si.name AS sales_invoice,
+                si.patient AS patient,
+                si.patient_name AS patient_name,
+                si.posting_date AS posting_date,
+                si.posting_time AS posting_time,
+                sii.amount AS amount,
+                sii.net_amount AS net_amount,
+                si.ref_physician AS physician,
+                si.sales_partner AS sales_partner
+            FROM `tabSales Invoice Item` as sii
+            LEFT JOIN `tabSales Invoice` AS si ON si.name = sii.parent
+            WHERE {clauses}
+        """.format(
+            clauses=_get_clauses(filters)
+        ),
+        as_dict=1,
+        values=filters,
+    )
+
+    def set_datetime(item):
+        return merge(
+            item,
+            {
+                "posting_datetime": (
+                    datetime.combine(item.get("posting_date"), datetime.min.time())
+                    + item.get("posting_time")
+                    if item.get("posting_date")
+                    else ""
+                )
+            },
+        )
+
+    return map(set_datetime, data)
+
+
+def _get_clauses(filters):
+    conditions = [
+        "si.docstatus = 1",
+        "posting_date BETWEEN %(from_date)s AND %(to_date)s",
+    ]
+    if filters.get("patient"):
+        conditions += ["si.patient = %(patient)s"]
+    if filters.get("physician"):
+        conditions += ["si.ref_physician = %(physician)s"]
+    if filters.get("sales_partner"):
+        conditions += ["si.sales_partner = %(sales_partner)s"]
+    return " AND ".join(conditions)
